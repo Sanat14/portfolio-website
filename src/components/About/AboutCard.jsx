da4e3c35e83a6fad1b1b42451a9586ffc9d8496b
Sanat <dusadsanat@gmail.com>
2025-07-04 17:46:17 +0100

    updating about section

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { FaBasketballBall, FaGamepad} from 'react-icons/fa';
 import { MdLocationOn } from 'react-icons/md';
+import { GiPokerHand } from 'react-icons/gi';
 
 
 function AboutCard() {
@@ -10,13 +11,11 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>  
-            I am currently in my Final year studying <strong className="purple">Computer Science with Data Science</strong> at <strong className="purple">University College Dublin</strong>. 
-            I am a passionate Software Engineer with a strong interest in Data Science. Currently playing around with ML/DL architectures  
-            to build fun and innovative projects.  
-            I am always eager to learn, adapt, and push the boundaries of technology.
 
+            I am currently working as a <strong className="purple">Software Engineer</strong> at <strong className="purple">Microsoft</strong>. Outside of work, I have a strong interest in Data Science and love tinkering with machine learning and deep learning models just for the fun of it. 
+            Whether it's building cool side projects, trying out new architectures, or just exploring what's possible with AI, I'm always up for learning something new and turning ideas into reality.
             <br /><br />
-            Besides Work and College I have a few hobbies :
+            I also have a few hobbies :
           </p>
           <ul>
             <li className="about-activity">
@@ -24,6 +23,9 @@ function AboutCard() {
             </li>
             <li className="about-activity">
               <FaGamepad  style={{color: "#c95bf5"}}/> &nbsp; Gaming
+            </li>
+            <li className="about-activity">
+              <GiPokerHand style={{color: "#c95bf5"}} /> &nbsp; Poker
             </li>
             <li className="about-activity">
               <MdLocationOn style={{color: "#c95bf5"}} /> &nbsp; Travelling
